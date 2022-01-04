6e81c5f7ae24d9a5fd7c5fd7222c1fcf591c7b0a
MuRing <33271901+Muring@users.noreply.github.com>
2022-01-04 22:46:30 +0900

    Time: 79 ms (5.35%), Space: 7 MB (97.38%) - LeetHub

--- 704-binary-search/704-binary-search.c
+++ 704-binary-search/704-binary-search.c
@@ -1,15 +1,17 @@
 int search(int* nums, int numsSize, int target){
     int i;
     int count = 0;
+    int ans;
     
     for(i = 0 ; i < numsSize ; i ++){
         if(nums[i] == target){
             count = 1;
+            ans = i;
             break;
         }
     }
     if(count == 1)
-        return i;
+        return ans;
     else
         return -1;
 }
