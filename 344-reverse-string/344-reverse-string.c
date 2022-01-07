a94bb10e6318df66c533a652b31d36877fd40ce1
MuRing <33271901+Muring@users.noreply.github.com>
2022-01-07 21:31:12 +0900

    Time: 75 ms (14.57%), Space: 12.4 MB (44.70%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.c
@@ -0,0 +1,16 @@
+void swap(char *a, char *b){
+    char temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void reverseString(char* s, int sSize){
+    int i;
+    int point = sSize - 1;
+
+    
+    for(i = 0 ; i < sSize / 2 ; i++)
+        swap(&s[i], &s[point - i]);
+    
+}
