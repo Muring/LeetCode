78243d577e597646f39694b472981ca2d77b74c1
MuRing <33271901+Muring@users.noreply.github.com>
2022-01-11 22:23:13 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (81.06%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.c
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+    struct ListNode *back = malloc(sizeof(struct ListNode));
+    struct ListNode *front = malloc(sizeof(struct ListNode));
+    struct ListNode *Node = malloc(sizeof(struct ListNode));
+    int i, count = 0;
+    
+    Node->val = -1;
+    Node->next = head;
+    back = Node;
+    front = Node;
+    
+    for(i = 0 ; i < n ; i++)
+        front = front->next;
+    
+    while(front->next != NULL){
+        front = front->next;
+        back = back->next;
+    }
+    back->next = back->next->next;
+    
+    return Node->next;
+}
