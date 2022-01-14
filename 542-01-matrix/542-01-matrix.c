417a61cc64b9be6770377642443b756e9481261e
MuRing <33271901+Muring@users.noreply.github.com>
2022-01-14 23:21:17 +0900

    Time: 1704 ms (5.68%), Space: 28.8 MB (19.32%) - LeetHub

--- 542-01-matrix/542-01-matrix.c
+++ 542-01-matrix/542-01-matrix.c
@@ -1,6 +1,6 @@
-void getDistance(int ** mat, int ** result, int row, int col, int x, int y, int range)
+void getDistance(int** mat, int **result, int row, int col, int x, int y, int range)
 {
-    if(x < 0 || x >= row || y < 0 || y >= col) return;
+    if(x >= row || x < 0 || y >= col || y < 0) return;
     if((range == 0 || mat[x][y] == 1) && (result[x][y] == 0 || result[x][y] > range))
     {
         result[x][y] = range;
@@ -11,18 +11,26 @@ void getDistance(int ** mat, int ** result, int row, int col, int x, int y, int 
     }
 }
 
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
 int** updateMatrix(int** mat, int matSize, int* matColSize, int* returnSize, int** returnColumnSizes){
-    int ** result = (int **)calloc(matSize*(*matColSize), sizeof(int *));
+    int ** result = (int **)calloc(matSize * (*matColSize), sizeof(int *));
     *returnSize = matSize;
     *returnColumnSizes = (int *)malloc(matSize*sizeof(int));
+    
     for(int i=0; i<matSize; i++)
     {
-        (*returnColumnSizes)[i] = *matColSize;
-        result[i] = (int *)calloc(*matColSize, sizeof(int));
+        (*returnColumnSizes)[i] = *matColSize;  //returnColumnSizes의 열 맞추기
+        result[i] = (int *)calloc(*matColSize, sizeof(int));    //result 배열 천제를 0으로 초기화
     }
+    
     for(int i=0; i<matSize; i++)
         for(int j=0; j<*matColSize; j++)
             if(mat[i][j] == 0)
                 getDistance(mat, result, matSize, *matColSize, i, j, 0);
+    
     return result;
 }
